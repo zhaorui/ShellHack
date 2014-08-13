d6c3ceddc38358bd940693b7bb7f8173ca42c3cb
zhaorui <zhaoruiexplorer@gmail.com>
2014-08-13 11:04:07 +0800

    add c language for creating tags, and some update on test.sh. test.sh should be in the Playground repo, I will move it later

--- test.sh
+++ test.sh
@@ -8,6 +8,11 @@ GetHomeDir()
 {
     # The home directory is the 6th field in the NSS data.
     getent passwd "$1" | cut -d : -f 6
+    Flag=1
+    if [ $Flag -eq 1 ]
+    then
+        echo Flag == 1
+    fi
 }
 
 #Main Sub
