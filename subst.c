6fb61d09a56b675ad38834d97c1ddddfde4c68e3
zhaorui <zhaoruiexplorer@gmail.com>
2015-07-08 00:00:52 +0800

    tired...

--- subst.c
+++ subst.c
@@ -1,31 +1,58 @@
 //subst <file> <old> <new>
 #define _POSIX_C_SOURCE 200809L
 #define BUFFER_LEN 4096
+#define MAX_LINE   1024
+#define FILE_MODE   (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
 
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {
-    char buffer[4096]; // change the 4096
-
+    int fd, newfd;
     const char* file = argv[1];
-    const char* old = argv[2];
-    const char* new = argv[3];
-    int fd;
-    if ((fd = open(file, O_RDWR|O_APPEND)) < 0){
-        perror("open failed");
+    char* old = (char*)malloc(strlen(argv[2])+1);
+    char* new = (char*)malloc(strlen(argv[3])+1);
+    int newfile_len = strlen(file)+strlen(".in");
+    char* newfile = (char *)malloc(newfile_len+1);
+    snprintf(newfile, newfile_len+1, "%s.in", file);
+
+    FILE *fp = fopen(file, "r+");
+    if(!fp){
+        perror("fopen failed");
         exit(1);
     }
 
-    //while(read(file, buffer, 4096)>0){
-    //    
-    //}
+    if ((newfd = open(newfile, O_CREAT|O_EXCL|O_APPEND, FILE_MODE)) < 0){
+        perror("open newfile failed");
+        exit(1);
+    }
 
-    write(fd, "hellworld", 9);
+    char* line = NULL;
+    size_t line_len = 0;
+    while (getline(&line, &line_len, fp) != -1){
+        printf("get the line: %s", line);
+        if (line[line_len-1] == '\n'){
+            
+        }
+        if ( line_len == strlen(old) && strncmp(line, old, line_len) == 0){
+            write(newfd, new, strlen(new));
+            if(line)
+        }
+        else
+            write(newfd, line, strlen(line));
+    }
 
+    free(line);
     close(fd);
+    close(newfd);
+
+    char cmd[MAX_LINE];
+    //snprintf(cmd, MAX_LINE, "mv %s %s", file, newfile);
+    //system(cmd);
+    free(newfile);
     return 0;
 }
